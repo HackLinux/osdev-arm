b290e3fdd68a656a6a712779822445c5d015728c
madan <madanpandiri@gmail.com>
2012-03-29 00:22:39 +0530

    proper interrupt handling

--- interrupt.c
+++ interrupt.c
@@ -1,6 +1,11 @@
 #include "print.h"
+#include "interrupt.h"
 
-static intr_handler irq_h[32] = {};
+//static intr_handler vect_h[MAX_IRQ];
+#define MAX_IRQ 32
+struct IRQ_LIST  irq_list[MAX_IRQ];
+
+
 
 int handle_irq(int status)
 {
@@ -16,7 +21,54 @@ int handle_fiq(int status)
 
 }
 
-
 int request_irq(int irqno, int mode, intr_handler h)
 {
+	if (irqno > MAX_IRQ) {
+		printk("Invalid irq no = %d\n");
+		return 0;
+	}
+	if (irq_list[irqno].no != 0){
+		printk("Irq is in use\n");
+		return 0;
+	}
+
+	irq_list[irqno].no = 1;
+	irq_list[irqno].h  = h;	
+	irq_list[irqno].mode = mode;
+		
+	switch (mode) {
+		case IRQ_MODE:
+			enable_irq(1<<irqno);
+		break;
+
+		case FIQ_MODE:
+			enable_fiq(1<<irqno);
+		break;
+#if 0
+		case VECTOR_MODE:
+			if (!vect_h[irqno]){
+				vect_h[irqno] = h;
+				/*able_fiq(1<<irqno);*/
+				ret = 1;
+			}
+				
+		break;
+#endif
+		default:
+			printk("Invalid mode\n");
+		break;
+	}
+
+	return 1;
+}
+
+void free_irq(int irqno)
+{
+	if (irq_list[irqno].no != 1) {
+		printk("Irq is not registered\n");
+		return ;
+	}
+	irq_list[irqno].no = 0;
+	irq_list[irqno].h  = 0;
+	irq_list[irqno].mode = 0;
 }
--- interrupt.h
+++ interrupt.h
@@ -1,6 +1,20 @@
 #ifndef __INTERRUPT_H
 #define __INTERRUPT_H
 
+enum mode{
+IRQ_MODE = 1,
+FIQ_MODE = 2,
+VECTOR_MODE = 3,
+};
+
 typedef int (*intr_handler)(int, void *);
+
+struct IRQ_LIST {
+	int no;
+	intr_handler h;
+	enum mode mode;
+};
+
 int request_irq(int irq, int type, int (*intr_handler)(int irq, void *data));
+
 #endif
